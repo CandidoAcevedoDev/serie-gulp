4c98317ea5f7fd01259ff819999c0d28222a942e
Candido Acevedo <candidoacevedodev@gmail.com>
2021-06-12 17:11:50 -0400

    Configurando Gulp por primera vez.

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3,4,5,6];
+const numberDouble = numbers.map(number => number * 2);
+
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+console.log(numbers);
---
+++ src/js/second.js
@@ -0,0 +1,6 @@
+const numbers2 = [7,8,3,10,500,60];
+const numbertriple = numbers.map(number => number * 3);
+
+console.log('Hola Mundo');
+console.log('Hola Candido');
+console.log('Hola Candido');
